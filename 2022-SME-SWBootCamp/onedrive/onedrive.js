34542cd33422e95b2842fe8c03262b84e1441bdd
Chaewon <cw001121@naver.com>
2022-07-24 03:15:44 +0900

    css add & hover add

--- 2022-SME-SWBootCamp/onedrive/onedrive.js
+++ 2022-SME-SWBootCamp/onedrive/onedrive.js
@@ -1,1 +1,9 @@
+// $('.aside-div').on({
+//     mousemove: function (event) {
+//         $('i.aside-div-icon').css("color", "red");
+//     },
+//     mouseout: function () {
+//         $('i.aside-div-icon').css("color", "");
+//     }
+// });
 
