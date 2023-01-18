8ba09b356b7b1c59e933f7db790487e350b76411
Chaewon <cw001121@naver.com>
2023-01-18 17:14:23 +0900

    fix up contents

---
+++ WEB/vue/ddaom_1/src/views/MakeProjectView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>프로젝트생성</h1>
+  </div>
+</template>
+<script>
+export default {
+  conponents: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
