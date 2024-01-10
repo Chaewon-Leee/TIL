46eb4cc2ec3c680d6275d7db8c49f061c96953ec
Chaewon <cw001121@naver.com>
2024-01-10 12:56:00 +0900

    upload : vanishing gradient

--- DL/vanishing_gradient/multiclass_functions1.py
+++ DL/vanishing_gradient/multiclass_functions1.py
@@ -5,62 +5,20 @@ import numpy as np # confusion matrix 사용시
 import matplotlib.pyplot as plt
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-def Train(model, train_DL, criterion, optimizer, LR, EPOCH):
-    # train_DL : train dataloader, optimizer : Adam
-    NoT=len(train_DL.dataset) # Number of training data
-    loss_history = []
-
-    model.train() # train mode로!
-    for ep in range(EPOCH):
-        rloss = 0
-        for x_batch, y_batch in train_DL:
-            # 32개를 기준으로 끊어 리턴 > 마지막이 32개 미만일 경우에도 그냥 내보냄
-            x_batch = x_batch.to(DEVICE)
-            y_batch = y_batch.to(DEVICE)
-            # inference
-            y_hat = model(x_batch)
-            # cross entropy loss
-            loss = criterion(y_hat, y_batch)
-            # update
-            optimizer.zero_grad() # gradient 누적을 막기 위한 초기화
-            loss.backward() # backpropagation
-            optimizer.step() # weight update
-            # loss accumulation
-            loss_b = loss.item() * x_batch.shape[0] # batch loss
-            # BATCH_SIZE 만큼 Loss에 곱채줌
-            # BATCH_SIZE 로 하면 마지막 16개도 32개로 계산해버림
-            rloss += loss_b # running loss = loss 총합
-        # print loss
-        loss_e = rloss/NoT # epoch loss
-        # 각 batch 개수별로 loss를 곱해줬으므로, 최종 loss 평균은 총 데이터 개수로 나누어줌
-        loss_history += [loss_e]
-        print(f"Epoch: {ep+1} train loss: {round(loss_e,3)}")
-        print("-"*20)
-
-    return loss_history
-
 def Test(model,test_DL):
     model.eval()
     with torch.no_grad():
         rcorrect = 0
-        # rloss = 0
         for x_batch, y_batch in test_DL:
             x_batch = x_batch.to(DEVICE)
             y_batch = y_batch.to(DEVICE)
             # inference
             y_hat = model(x_batch)
-            # loss
-            # loss = criterion(y_hat, y_batch) # 만약 쓰려면 매개변수에 criterion 추가
-            # loss accumulation
-            # loss_b = loss.item() * x_batch.shape[0] # BATCH_SIZE 로 하면 마지막 16개도 32개로 계산해버림
-            # rloss += loss_b # running loss
             # accuracy accumulation
-            pred = y_hat.argmax(dim=1)
+            pred = y_hat.argmax(dim=1) # 10개 중 가장 높은 값의 인덱스 = 예측한 숫자 인덱스
             corrects_b = torch.sum(pred == y_batch).item()
-            rcorrect += corrects_b
-        # loss_e = rloss/NoTes
+            rcorrect += corrects_b # 맞았을 경우, 추가
         accuracy_e = rcorrect/len(test_DL.dataset)*100
-    # print("test loss: ", round(loss_e,2))
     print(f"Test accuracy: {rcorrect}/{len(test_DL.dataset)} ({round(accuracy_e,1)} %)")
     return round(accuracy_e,1)
 
@@ -86,50 +44,4 @@ def count_params(model):
     num = sum([p.numel() for p in model.parameters() if p.requires_grad])
     return num
 
-
-def get_conf(model, test_DL):
-    model.eval()
-    with torch.no_grad():
-        confusion = torch.zeros(10,10)
-        for x_batch, y_batch in test_DL:
-            x_batch = x_batch.to(DEVICE)
-            y_batch = y_batch.to(DEVICE)
-            # inference
-            y_hat = model(x_batch)
-            # accuracy
-            pred = y_hat.argmax(dim=1)
-
-            for i in range(y_batch.shape[0]):
-                confusion[pred[i], y_batch[i]] += 1
-            # confusion[pred, y_batch] += 1
-
-    confusion = confusion.numpy()
-
-    return confusion
-
-def plot_confusion_matrix(confusion, classes=None):
-    accuracy=np.trace(confusion)/np.sum(confusion) * 100
-
-    confusion = confusion/np.sum(confusion, axis=0)
-    plt.figure(figsize=(15,10))
-    plt.imshow(confusion, cmap="Blues")
-    plt.title("confusion matrix")
-    plt.colorbar()
-
-    for i in range(confusion.shape[0]):
-        for j in range(confusion.shape[1]):
-            plt.text(j,i, round(confusion[i,j],2),
-                     horizontalalignment="center",
-                     color="white" if confusion[i,j] > np.max(confusion) / 1.5 else "black")
-
-    if classes is not None:
-        plt.xticks(range(10), classes)
-        plt.yticks(range(10), classes)
-    else:
-        plt.xticks(range(10))
-        plt.yticks(range(10))
-
-    plt.ylabel("Predicted label")
-    plt.xlabel(f"True label \n accuracy = {round(accuracy,1)} %")
-    plt.show()
 
